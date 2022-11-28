0fa872a9c3c06845716e12e4b7a716af6c057a26
abdulrahman3mad <abdulrahman3mad@gmail.com>
2022-11-28 17:28:37 +0200

    Add Theme & Utilities Style & set the main structure of files (components, pages, section, sass)

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import "./sass/index.scss";
+import "bootstrap/dist/js/bootstrap.bundle.min.js"
+import "bootstrap/dist/css/bootstrap.min.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
