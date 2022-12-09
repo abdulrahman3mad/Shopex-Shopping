914fab6f74df24169755b2a80b198dd58de2b5c2
Hisham khaled <hishamk1999@gmail.com>
2022-12-09 16:03:04 +0200

    Finsh Featured Products Section
    -create Color palette "component"
    -create Featured Pro Card "component"
    -Create Price "component"
    -Sass-Components(_ColorPalette.scss,_FeaProCard.scss,_Price.scss)
    -Sass-Sections(_FeaturedSE.scss)
    -create Featured Data Slice
    -Add (npm i install react-slick --save)
    -Add (npm install slick-carousel --save)

---
+++ client/src/components/ColorPalette/ColorPalette.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ColorPalette() {
+	return (
+		<div className="color-palette d-flex justify-content-center mb-3">
+			<span className="color A"></span>
+			<span className="color B"></span>
+			<span className="color C"></span>
+		</div>
+	);
+}
+
+export default ColorPalette;
---
+++ client/src/components/Price/Price.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Price({ val }) {
+	return (
+		<div className="price">
+			<h5>${val}</h5>
+		</div>
+	);
+}
+
+export default Price;
