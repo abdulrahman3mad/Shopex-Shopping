9a135a43e6983379bab10356d87d4b4a06bea167
Abdulrahman Emad <65741884+abdulrahman3mad@users.noreply.github.com>
2022-12-11 13:58:44 +0200

    adding notFound page

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import LogIn from "./pages/LogIn/LogIn";
 import Home from "./pages/Home/Home"
 // sections
 import HeaderNav from "./sections/UpperBar/UpperBar";
+import NotFound from "./pages/NotFound/NotFound";
 
 function App() {
 	return (
@@ -14,6 +15,7 @@ function App() {
 				<Route path="/" element={<Home />}></Route>
 				<Route path="/login" element={<LogIn />}></Route>
 				<Route path="/signup" element={<SignUp />}></Route>
+				<Route path="*" element={<NotFound />}></Route>
 			</Routes>
 		</div>
 	);
