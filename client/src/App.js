ba23785e9893d560225a30216e47fc70bca9446c
Hisham khaled <hishamk1999@gmail.com>
2022-12-13 13:26:16 +0200

    Finish Newsletter Section -Add NewsletterSE.jsx "Sections". -Add Sass(sec:_Newsletter.scss) -Moved header and footer sections from home.js to ->> app.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,20 @@ import LogIn from "./pages/LogIn/LogIn";
 import Home from "./pages/Home/Home";
 
 import NotFound from "./pages/NotFound/NotFound";
+import HeaderNav from "./sections/UpperBar/UpperBar";
+import FooterSE from "./sections/Footer/FooterSE";
 
 function App() {
 	return (
 		<div className="App">
+			<HeaderNav />
 			<Routes>
 				<Route path="/" element={<Home />}></Route>
 				<Route path="/login" element={<LogIn />}></Route>
 				<Route path="/signup" element={<SignUp />}></Route>
 				<Route path="*" element={<NotFound />}></Route>
 			</Routes>
+			<FooterSE />
 		</div>
 	);
 }
