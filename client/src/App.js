3d8208bb04485246e9ceedae2f4ceddc7db717e0
Hisham khaled <hishamk1999@gmail.com>
2022-12-21 16:22:13 +0200

    Fix path files import

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,19 @@
+import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
 // pages
-import SignUp from "./pages/SignUp/SignUp";
-import LogIn from "./pages/LogIn/LogIn";
-import NotFound from "./pages/NotFound/NotFound";
-import Home from "./pages/Home/Home";
-import Shop from "./pages/Shop/Shop";
-import HeaderNav from "./sections/UpperBar/UpperBar";
-import FooterSE from "./sections/Footer/FooterSE";
-import Dashboard from "./pages/Dashboard/Dashboard";
-import Cart from "./pages/Cart/Cart";
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
-import { setUserData } from "./redux-toolkit/features/userSlices/userSlice";
-import { getCart } from "./redux-toolkit/features/cartSlices/cartSlice";
+import SignUp from "pages/SignUp/SignUp";
+import LogIn from "pages/LogIn/LogIn";
+import NotFound from "pages/NotFound/NotFound";
+import Home from "pages/Home/Home";
+import Shop from "pages/Shop/Shop";
+import HeaderNav from "sections/UpperBar/UpperBar";
+import FooterSE from "sections/Footer/FooterSE";
+import Dashboard from "pages/Dashboard/Dashboard";
+import Cart from "pages/Cart/Cart";
+import { setUserData } from "redux-toolkit/features/userSlices/userSlice";
+import { getCart } from "redux-toolkit/features/cartSlices/cartSlice";
 
 function App() {
 	const { user, cart } = useSelector((state) => state);
@@ -26,7 +26,6 @@ function App() {
 	useEffect(() => {
 		if (user && Object.keys(user).length) dispatch(getCart());
 	}, [user, dispatch]);
-
 
 	return (
 		<div className="App">
