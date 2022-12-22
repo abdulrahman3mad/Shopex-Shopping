89a63e986472d36bf35cc50b74972d006e4c8f4a
Abdulrahman Emad <65741884+abdulrahman3mad@users.noreply.github.com>
2022-12-22 13:47:38 +0200

    edits in routes in indexes files

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,8 +1,8 @@
-export { default as Cart } from "./Cart/Cart";
-export { default as Home } from "./Home/Home";
-export { default as LogIn } from "./LogIn/LogIn";
-export { default as SignUp } from "./SignUp/SignUp";
-export { default as NotFound } from "./NotFound/NotFound";
-export { default as Shop } from "./Shop/Shop";
-export { default as Contact } from "./Contact/Contact";
-export { default as Dashboard } from "./Dashboard/Dashboard";
+export { default as Cart } from "pages/Cart/Cart";
+export { default as Home } from "pages/Home/Home";
+export { default as LogIn } from "pages/LogIn/LogIn";
+export { default as SignUp } from "pages/SignUp/SignUp";
+export { default as NotFound } from "pages/NotFound/NotFound";
+export { default as Shop } from "pages/Shop/Shop";
+export { default as Contact } from "pages/Contact/Contact";
+export { default as Dashboard } from "pages/Dashboard/Dashboard";
